7960255c175c6e88f04c3c90da6fccc9d50d2f98
YudaiShiratori <toritorikebabu1023x@gmail.com>
2019-06-15 13:44:40 +0900

    functions_request

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,31 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
+// import { response } from 'express';
 
 admin.initializeApp()
-export const holaworld = functions.https.onRequest((request, response) => {
-  response.send('Hola')
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello')
+})
+
+export const requestTest = functions.https.onRequest((request, response) => {
+  console.log('method', request.method, 'content-type', request.headers['content-type'])
+  const resultParam: any = { message: '' }
+  let responseCode: number = 200
+  try {
+    const data = request.body
+    const id: string = data !== undefined && 'id' in data? data.id : undefined
+    if (request.method === 'GET') {
+      resultParam.message = 'REQUEST method is GET'
+    } else if (request.method === 'POST') {
+      resultParam.message = `REQUEST method is POST. body is ${id}`
+    } else if (request.method === 'PUT') {
+      resultParam.message = 'REQUEST method id POST'
+    } else if (request.method === 'DELETE') {
+      resultParam.message = 'REQUEST method is DELETE'
+    } 
+  } catch (error) {
+    responseCode = 500
+    resultParam.message = error.message
+  }
+  response.status(responseCode).send({ code: responseCode, result: resultParam })
 })
